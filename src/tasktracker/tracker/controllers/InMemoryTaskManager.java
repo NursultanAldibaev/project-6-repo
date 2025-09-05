4aa96e2d966f88d0e67bf7fce5d0ea1fed06e91b
NAldibayev <aldibaevnursultan@yandex.ru>
2025-09-05 12:57:03 +0500

    Fix Checkstyle: adjust braces in InMemoryTaskManager

--- src/tasktracker/tracker/controllers/InMemoryTaskManager.java
+++ src/tasktracker/tracker/controllers/InMemoryTaskManager.java
@@ -17,102 +17,86 @@ public class InMemoryTaskManager implements TaskManager {
     private final Map<Integer, Task> tasks = new HashMap<>();
     private final Map<Integer, Epic> epics = new HashMap<>();
     private final Map<Integer, Subtask> subtasks = new HashMap<>();
-
     private int nextId = 1;
 
     @Override
-    public int createTask(Task task)
-    {
+    public int createTask(Task task) {
         task.setId(nextId++);
         tasks.put(task.getId(), task);
         return task.getId();
     }
 
     @Override
-    public int createEpic(Epic epic)
-    {
+    public int createEpic(Epic epic) {
         epic.setId(nextId++);
         epics.put(epic.getId(), epic);
         return epic.getId();
     }
 
     @Override
-    public int createSubtask(Subtask subtask)
-    {
+    public int createSubtask(Subtask subtask) {
         subtask.setId(nextId++);
         subtasks.put(subtask.getId(), subtask);
         return subtask.getId();
     }
 
     @Override
-    public Task getTaskById(int id)
-    {
+    public Task getTaskById(int id) {
         return tasks.get(id);
     }
 
     @Override
-    public Epic getEpicById(int id)
-    {
+    public Epic getEpicById(int id) {
         return epics.get(id);
     }
 
     @Override
-    public Subtask getSubtaskById(int id)
-    {
+    public Subtask getSubtaskById(int id) {
         return subtasks.get(id);
     }
 
     @Override
-    public List<Task> getAllTasks()
-    {
+    public List<Task> getAllTasks() {
         return new ArrayList<>(tasks.values());
     }
 
     @Override
-    public List<Epic> getAllEpics()
-    {
+    public List<Epic> getAllEpics() {
         return new ArrayList<>(epics.values());
     }
 
     @Override
-    public List<Subtask> getAllSubtasks()
-    {
+    public List<Subtask> getAllSubtasks() {
         return new ArrayList<>(subtasks.values());
     }
 
     @Override
-    public void updateTask(Task task)
-    {
+    public void updateTask(Task task) {
         tasks.put(task.getId(), task);
     }
 
     @Override
-    public void updateEpic(Epic epic)
-    {
+    public void updateEpic(Epic epic) {
         epics.put(epic.getId(), epic);
     }
 
     @Override
-    public void updateSubtask(Subtask subtask)
-    {
+    public void updateSubtask(Subtask subtask) {
         subtasks.put(subtask.getId(), subtask);
     }
 
     @Override
-    public void deleteTask(int id)
-    {
+    public void deleteTask(int id) {
         tasks.remove(id);
     }
 
     @Override
-    public void deleteEpic(int id)
-    {
+    public void deleteEpic(int id) {
         epics.remove(id);
     }
 
     @Override
-    public void deleteSubtask(int id)
-    {
+    public void deleteSubtask(int id) {
         subtasks.remove(id);
     }
 
