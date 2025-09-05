72186c9a70efef11a6db099e424fb9b733986def
NAldibayev <aldibaevnursultan@yandex.ru>
2025-09-05 11:10:45 +0500

    fix: clean Managers.java syntax and remove invalid symbols

--- src/tasktracker/tracker/controllers/Managers.java
+++ src/tasktracker/tracker/controllers/Managers.java
@@ -1,1 +1,20 @@
-... (вставь здесь Managers.java) ...
+package tasktracker.tracker.controllers;
+
+/**
+ * Утилитарный класс для получения реализаций менеджеров.
+ */
+public final class Managers {
+
+    private Managers() {
+        // запрет на создание экземпляра
+    }
+
+    public static TaskManager getDefault() {
+        return new InMemoryTaskManager();
+    }
+
+    public static HistoryManager getDefaultHistory() {
+        return new InMemoryHistoryManager();
+    }
+}
+
