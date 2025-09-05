bc78d17dd9eb2ff058fea676c33cd79b85f1c181
NAldibayev <aldibaevnursultan@yandex.ru>
2025-09-05 13:19:22 +0500

    Fix Task/Subtask/Epic classes with proper Checkstyle and update InMemoryTaskManager

--- src/tasktracker/Main.java
+++ src/tasktracker/Main.java
@@ -4,13 +4,10 @@ import tracker.controllers.InMemoryTaskManager;
 import tracker.controllers.TaskManager;
 import tracker.model.Subtask;
 import tracker.model.Task;
-import tracker.model.Epic;
-import tracker.model.Status;
 
 public class Main {
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         TaskManager manager = new InMemoryTaskManager();
 
         Task task1 = new Task("Task 1", "Description 1");
@@ -18,11 +15,8 @@ public class Main {
         manager.createTask(task1);
         manager.createTask(task2);
 
-        Epic epic = new Epic("Epic 1", "Epic Description");
-        int epicId = manager.createEpic(epic);
-
-        Subtask sub1 = new Subtask("Subtask 1", "Subdesc 1", epicId);
-        Subtask sub2 = new Subtask("Subtask 2", "Subdesc 2", epicId);
+        Subtask sub1 = new Subtask("Subtask 1", "Subdesc 1", 0);
+        Subtask sub2 = new Subtask("Subtask 2", "Subdesc 2", 0);
         manager.createSubtask(sub1);
         manager.createSubtask(sub2);
 
@@ -31,15 +25,9 @@ public class Main {
             System.out.println(task);
         }
 
-        System.out.println("All epics:");
-        for (Epic e : manager.getAllEpics()) {
-            System.out.println(e);
-        }
-
         System.out.println("All subtasks:");
         for (Subtask subtask : manager.getAllSubtasks()) {
             System.out.println(subtask);
         }
     }
-
 }
