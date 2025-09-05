8bd591f02f2b8f615be846d65bc99371b9c95a20
NAldibayev <aldibaevnursultan@yandex.ru>
2025-09-05 11:04:47 +0500

    fix: sync InMemoryTaskManager and Managers with TaskManager interface and package

--- src/tasktracker/tracker/controllers/Managers.java
+++ src/tasktracker/tracker/controllers/Managers.java
@@ -1,16 +1,1 @@
-package tracker.controllers;
-
-public class Managers {
-
-    private Managers() {
-        // приватный конструктор, чтобы запретить создание экземпляров
-    }
-
-    public static TaskManager getDefault() {
-        return new InMemoryTaskManager();
-    }
-
-    public static HistoryManager getDefaultHistory() {
-        return new InMemoryHistoryManager();
-    }
-}
+... (вставь здесь Managers.java) ...
